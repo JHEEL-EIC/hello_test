164c7f940bfcbcb3750c2350a9c924bd65d0ddd9
JHEEL-EIC <jheel.shah@einfochips.com>
2025-05-26 14:36:28 +0530

    Added filter even functionality

--- helloProgram.cpp
+++ helloProgram.cpp
@@ -1,7 +1,34 @@
 /*---------hello world--------*/
 #include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void filterEven(vector<int>&arr)
+{
+	if(arr.empty())
+	{
+		cout<<"vector is empty"<<endl;
+	}
+	arr.erase(remove_if(arr.begin(), arr.end(), [](int n){ return n%2 != 0; } ), arr.end());
+}
+void print(vector<int>&arr)
+{
+	for(auto it : arr)
+        {
+                cout<<it<<" ";
+        }
+	cout<<endl;
+}
 int main()
 {
-	std::cout<<"HELLO WORLD"<<std::endl;
+	cout<<"HELLO WORLD"<<endl;
+	//vector input
+	vector<int>vec_even = {1, 2, 3, 4, 5, 6};
+	cout<<"input: ";
+	print(vec_even);
+	//filter even nums from vector
+	filterEven(vec_even);
+	cout<<"filtered even: ";
+	print(vec_even);
 	return 0;
 }
